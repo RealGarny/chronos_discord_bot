eb61c827e26341a96ff5ddd4c72f09e5834b103d
realgarny <aseev.nik1@yandex.ru>
2023-10-04 22:19:07 +0500

    first day changes

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+load_dotenv()
+DISCORD_TOKEN = os.getenv('BOT_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run('my token goes here')
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    print(f"User {message.author.name} Said: {message.content}")
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(DISCORD_TOKEN)
