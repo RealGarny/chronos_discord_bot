e1062fcc3588e5a9d8194e5e4ac9f8dfa7ccee5a
realgarny <aseev.nik1@yandex.ru>
2023-10-08 15:43:25 +0500

    starting fresh

--- app.py
+++ app.py
@@ -2,32 +2,17 @@ import discord
 from discord.ext import commands
 from settings import DISCORD_TOKEN
 
-intents = discord.Intents.default()
-client = commands.Bot(command_prefix="!", intents=intents)
+def main():
+    intents = discord.Intents.default()
 
+    bot = commands.Bot(command_prefix="!", intents=intents)
 
-@client.command()
-async def hello(ctx):
-    await ctx.send("hello")
+    @bot.event
+    async def on_ready():
+        print(f"bot started as: {bot.user}")
+        print('-----------------------')
 
-'''
-intents = discord.Intents.default()
-intents.message_content = True
+    bot.run(DISCORD_TOKEN)
 
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    print(f"User {message.author.name} Said: {message.content}")
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-'''
 if __name__ == "__main__":
-    client.run(DISCORD_TOKEN)
+    main()
