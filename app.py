e36fde198d8b16517facc269b1bee1e31a3bfc3b
realgarny <aseev.nik1@yandex.ru>
2023-10-08 15:35:51 +0500

    created separate settings file

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import discord
 from discord.ext import commands
-import os
-from dotenv import load_dotenv
+from settings import DISCORD_TOKEN
 
-load_dotenv()
-DISCORD_TOKEN = os.getenv('BOT_TOKEN')
+intents = discord.Intents.default()
+client = commands.Bot(command_prefix="!", intents=intents)
 
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("hello")
+
+'''
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -23,5 +28,6 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-
-client.run(DISCORD_TOKEN)
+'''
+if __name__ == "__main__":
+    client.run(DISCORD_TOKEN)
---
+++ settings.py
@@ -0,0 +1,5 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv('BOT_TOKEN')
